442efab030fc587840601298727dcb548a8b1e2d
Arman Ali <toplinegeeks@gmail.com>
2021-12-01 16:38:58 +0500

    fixed the issue and assign custom scroll bar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,30 @@
 import React from "react";
 import "./home.css";
+import { Youtube } from "./Youtube";
 export const Home = () => {
   return (
     <div className="home">
-      <div className="home__video">
-        <img
-          src="https://louisem.com/wp-content/uploads/2017/06/youtube-thumbnail-FB.jpg"
-          alt=""
-          className="home__videoImg"
-        />
-        <div className="home__videoHeadline">
-          <img
-            src="https://yt3.ggpht.com/yti/APfAmoGohKC3_VJIpumrXtEn6KOnWu3QRoASDo3ozI3B=s108-c-k-c0x00ffffff-no-rj"
-            alt=""
-            className="home__videoHeadlineImg"
-          />
-          <h5 className="home__videoHeadlineTitle">
-            Zero to Full-Time Programmer in 5 Steps
-          </h5>
-        </div>
-
-        <p className="home__vidoeChannel">Village Food </p>
-        <div className="home__videoInfo">
-          <p className="home__videoInfoView">48k views</p>
-          <p className="home__videoInfoTime">3 hours ago</p>
-        </div>
-      </div>
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
+      <Youtube />
     </div>
   );
 };
