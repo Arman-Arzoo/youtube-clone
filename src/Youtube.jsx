4614788fdcc93fee416ef3171e90cb9511dc16d7
Arman Ali <toplinegeeks@gmail.com>
2021-12-01 15:09:02 +0500

    set router for different content

--- src/Youtube.jsx
+++ src/Youtube.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import "./youtube.css";
 import { Yheader } from "./components/Yheader.jsx";
 import { Ysidebar } from "./components/Ysidebar.jsx";
-
+import { BrowserRouter } from "react-router-dom";
 import { ContentArea } from "./components/ContentArea.jsx";
 
 export const Youtube = () => {
   return (
-    <div>
-      <Yheader />
-      <div className="content__main">
-        <Ysidebar />
-        <ContentArea />
+    <BrowserRouter>
+      <div>
+        <Yheader />
+        <div className="content__main">
+          <Ysidebar />
+          <ContentArea />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
--- src/components/Ysidebar.jsx
+++ src/components/Ysidebar.jsx
@@ -4,29 +4,37 @@ import HomeIcon from "@material-ui/icons/Home";
 import ExploreIcon from "@material-ui/icons/ExploreOutlined";
 import SubscribeIcon from "@material-ui/icons/SubscriptionsOutlined";
 import LibraryIcon from "@material-ui/icons/VideoLibraryOutlined";
+import { NavLink } from "react-router-dom";
 export const Ysidebar = () => {
   return (
     <div className="ySidebar">
       <div className="ySidebar__nav">
-        <div className="ysidebar__navOption">
-          <HomeIcon className="ySidebar__HomeIcon" />
-          <p className="ySidebar__text">Home</p>
-        </div>
+        <NavLink to="/">
+          <div className="ysidebar__navOption">
+            <HomeIcon className="ySidebar__HomeIcon" />
+            <p className="ySidebar__text">Home</p>
+          </div>
+        </NavLink>
 
-        <div className="ysidebar__navOption ">
-          <ExploreIcon className="ySidebar__HomeIcon" />
-          <p className="ySidebar__text">Explore</p>
-        </div>
+        <NavLink to="/explore">
+          <div className="ysidebar__navOption ">
+            <ExploreIcon className="ySidebar__HomeIcon" />
+            <p className="ySidebar__text">Explore</p>
+          </div>
+        </NavLink>
 
-        <div className="ysidebar__navOption">
-          <SubscribeIcon className="ySidebar__HomeIcon" />
-          <p className="ySidebar__text">Subscritions</p>
-        </div>
-
-        <div className="ysidebar__navOption">
-          <LibraryIcon className="ySidebar__HomeIcon" />
-          <p className="ySidebar__text">Library</p>
-        </div>
+        <NavLink to="/subscription">
+          <div className="ysidebar__navOption">
+            <SubscribeIcon className="ySidebar__HomeIcon" />
+            <p className="ySidebar__text">Subscriptions</p>
+          </div>
+        </NavLink>
+        <NavLink to="/Library">
+          <div className="ysidebar__navOption">
+            <LibraryIcon className="ySidebar__HomeIcon" />
+            <p className="ySidebar__text">Library</p>
+          </div>
+        </NavLink>
       </div>
     </div>
   );
